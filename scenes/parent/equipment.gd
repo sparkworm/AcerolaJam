2323c1c547ce27dd450bf4709089946fd040a17b
Sparkworm <imnota.squirp@gmail.com>
2024-03-08 10:28:00 -0600

    Fixed the cooldown on equipment.

--- scenes/parent/equipment.gd
+++ scenes/parent/equipment.gd
@@ -4,15 +4,23 @@ extends Node2D
 ## the amount of time in seconds that the equipment will have to wait
 @export var cooldown: float = 1.0
 
+var cooled_down: bool
+
 func _ready():
 	%UseCooldown.wait_time = cooldown
+	cooled_down = true
 
 func can_use() -> bool:
-	return %UseCooldown.is_stopped()
+	return cooled_down
 
 func use() -> void:
 	if can_use():
+		%UseCooldown.start()
+		cooled_down = false
 		execute_use()
 
 func execute_use() -> void:
 	pass
+
+func _on_use_cooldown_timeout():
+	cooled_down = true
