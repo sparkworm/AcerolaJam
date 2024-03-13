06d6a0e93ab168bc05a647d7886903ad90d8d9fb
Sparkworm <d.clutter2337@gmail.com>
2024-03-12 19:35:40 -0500

    - Added pistol enemy (this was forgotten)  - Changed some basic settings in the enemy superclass  - Changed preload to load in the autoload file (this was to prevent a hypothetical crash)

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -2,6 +2,6 @@ extends Node
 
 var levels: Dictionary = {
 	"Main Menu" : null,
-	"Test Level" : preload("res://scenes/levels/test_level.tscn"),
+	"Test Level" : load("res://scenes/levels/test_level.tscn"),
 	"Level 1" : load("res://scenes/levels/level_1.tscn"),
 }
