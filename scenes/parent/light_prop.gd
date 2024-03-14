9b4a89f95f439510385f0232626fad1782708630
Sparkworm <d.clutter2337@gmail.com>
2024-03-13 20:39:24 -0500

    - Made it so that light props will not repeatedly play the broken sound

--- scenes/parent/light_prop.gd
+++ scenes/parent/light_prop.gd
@@ -1,12 +1,16 @@
 extends StaticBody2D
 
+var broken: bool = false
+
 func _ready():
 	$PointLight2D.color = modulate
 
 func hit(_damage):
-	$BreakSound.play(0.2)
-	$Sprite2D.modulate = Color(0.1, 0.1, 0.1)
-	turn_off()
+	if not broken:
+		$BreakSound.play(0.2)
+		$Sprite2D.modulate = Color(0.1, 0.1, 0.1)
+		turn_off()
+		broken = true
 
 func turn_on() -> void:
 	$PointLight2D.enabled = true
