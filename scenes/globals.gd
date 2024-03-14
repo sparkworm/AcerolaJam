fdb2e11d59b968e0c12ce4998b0287ff23c86fe3
Sparkworm <d.clutter2337@gmail.com>
2024-03-13 19:18:24 -0500

    - Added a second level

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -4,4 +4,5 @@ var levels: Dictionary = {
 	"Main Menu" : null,
 	"Test Level" : load("res://scenes/levels/test_level.tscn"),
 	"Level 1" : load("res://scenes/levels/level_1.tscn"),
+	"Level 2" : load("res://scenes/levels/level_2.tscn")
 }
